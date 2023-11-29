7a18276ffde838efc52576c4ed8d5099ef6d27e3
zserinz <serinn1023@gmail.com>
2023-11-30 00:31:25 +0900

    [스택,큐] 주석 추가

--- src/stack_queue/RemoveDuplicateLetters.kt
+++ src/stack_queue/RemoveDuplicateLetters.kt
@@ -3,29 +3,34 @@ package stack_queue
 import java.util.ArrayDeque
 import java.util.Deque
 
+/**
+ * Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is
+ * the smallest in lexicographical order
+ *  among all possible results.
+ */
 class RemoveDuplicateLetters {
     fun removeDuplicateLetters(s: String): String {
-        val counter: MutableMap<Char, Int> = HashMap()
-        val seen: MutableMap<Char, Boolean> = HashMap()
-        val stack: Deque<Char> = java.util.ArrayDeque()
+        val counter: MutableMap<Char, Int> = HashMap() // 중복 카운트 -> 문자 개수를 계산해서 담아 둘 변수
+        val seen: MutableMap<Char, Boolean> = HashMap() // 이미 처리한 문자 여부 선언
+        val stack: Deque<Char> = java.util.ArrayDeque() // 문자를 쌓아 나갈 변수 -> 정답으로 제출할 변수
 
         for (c in s)
-            counter[c] = counter.getOrDefault(c,0) + 1
+            counter[c] = counter.getOrDefault(c,0) + 1 // 문자별 개수 계산
 
         for (c in s) {
-            counter[c] = counter[c]!! - 1
-            if (seen[c] == true)
+            counter[c] = counter[c]!! - 1 // 현재 처리하는 문자는 -1 처리
+            if (seen[c] == true) // 이미 처리한 문자는 건너 뛰기
                 continue
-            while (!stack.isEmpty() && stack.peek() > c && counter[stack.peek()]!! > 0)
+            while (!stack.isEmpty() && stack.peek() > c && counter[stack.peek()]!! > 0) // 현재 문자보다 더 뒤에 붙여야 할 문자이면 스택에서 제거하고 처리하지 않음 표시
                 seen[stack.pop()] = false
 
-            stack.push(c)
-            seen[c] = true
+            stack.push(c) // 현재 문자 스택에 삽입
+            seen[c] = true // 현재 문자 처리한 것으로 선언
         }
 
         val sb = StringBuilder()
         while (!stack.isEmpty())
-            sb.append(stack.pollLast())
+            sb.append(stack.pollLast()) // 스택에 있는 문자를 큐 순서대로 추출해서 선언
 
         return sb.toString()
     }
--- src/stack_queue/ValidParentheses.kt
+++ src/stack_queue/ValidParentheses.kt
@@ -3,9 +3,20 @@ package stack_queue
 import java.util.ArrayDeque
 import java.util.Deque
 
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ *
+ */
 class ValidParentheses {
     fun isValid(s: String): Boolean {
         val stack: Deque<Char> = java.util.ArrayDeque()
+        // 유효성 검증을 위한 매핑 테이블
         val table: Map<Char, Char> = mapOf(
             ')' to '(',
             '}' to '{',
@@ -13,12 +24,12 @@ class ValidParentheses {
         )
 
         for (i in s.indices) {
-            if (!table.containsKey(s[i])) {
+            if (!table.containsKey(s[i])) { // 닫힘 괄호가 아닌 경우 스택에 푸시
                 stack.push(s[i])
-            } else if (stack.isEmpty() || table[s[i]] !== stack.pop()) {
+            } else if (stack.isEmpty() || table[s[i]] !== stack.pop()) { // 괄호가 연속으로 존재하는 경우 제한
                 return false
             }
         }
-        return stack.size == 0
+        return stack.size == 0 // 최종 결과가 반복 완료 이후에 비어있어야 한다.
     }
 }
