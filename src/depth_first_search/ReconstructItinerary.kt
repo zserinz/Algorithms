20b008cf195f8f4cdc3c6f013a32e16da0e0e6ee
zserinz <serinn1023@gmail.com>
2023-12-11 22:57:31 +0900

    [그래프] 주석 추가

--- src/depth_first_search/ReconstructItinerary.kt
+++ src/depth_first_search/ReconstructItinerary.kt
@@ -5,22 +5,36 @@ import java.util.Deque
 import java.util.LinkedList
 import java.util.PriorityQueue
 
+/**
+ * You are given a list of airline tickets where tickets[i] = [from, to] represent the departure and the arrival airports of one flight. Reconstruct the itinerary in order and return it.
+ *
+ * All of the tickets belong to a man who departs from "JFK", thus, the itinerary must begin with "JFK". If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
+ *
+ * For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+ * You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
+ */
 class ReconstructItinerary {
     fun findItinerary(tickets: List<List<String>>): List<String> {
         val fromToMap: MutableMap<String, PriorityQueue<String>> = mutableMapOf()
+
+        // 여행 일정을 from -> to 형태의 그래프로 구성한다.
         for (ticket in tickets) {
-            fromToMap.putIfAbsent(ticket[0], PriorityQueue())
-            fromToMap[ticket[0]]!!.add(ticket[1])
+            fromToMap.putIfAbsent(ticket[0], PriorityQueue()) // 값이 존재하지 않을 경우 빈 우선순위 큐 생성
+            fromToMap[ticket[0]]!!.add(ticket[1]) // 목적지 to 추가, 우선순위 큐이므로 to는 항상 사전 어휘순으로 정렬된다.
         }
         val result: MutableList<String> = LinkedList()
         val stack: Deque<String> = ArrayDeque()
 
-        stack.push("JFK")
+        stack.push("JFK") // 출발지 삽입
+
         while (!stack.isEmpty()) {
+            // 스택에서 추출될 값을 출발지로 한 도착지를 처리한다.
             while (fromToMap.containsKey(stack.first) && !fromToMap[stack.first]!!.isEmpty()) {
+                // 여러 도착지 중에서 사전 어휘 순으로 추출하여 스택에 삽입한다.
                 stack.push(fromToMap[stack.first]!!.poll())
             }
 
+            // 위 반복문이 모두 실행된 이후에는 스택에서 값이 하나씩 추출되어 정답으로 구성된다.
             result.add(0, stack.poll())
         }
         return result
--- src/depth_first_search/Subsets.kt
+++ src/depth_first_search/Subsets.kt
@@ -3,20 +3,28 @@ package depth_first_search
 import java.util.ArrayDeque
 import java.util.Deque
 
+/**
+ * Given an integer array nums of unique elements, return all possible
+ * subsets
+ *  (the power set).
+ *
+ * The solution set must not contain duplicate subsets. Return the solution in any order.
+ */
 class Subsets {
     fun subsets(nums: IntArray): List<List<Int>> {
         val results: MutableList<List<Int>> = mutableListOf()
 
         fun dfs(index: Int, path: Deque<Int>) {
-            results.add(ArrayList(path))
+            results.add(ArrayList(path)) // 모든 탐색 경로를 매번 결과에 추가한다.
+            // 자기보다 큰 숫자를 DFS 진행한다.
             for (i in index until nums.size) {
-                path.add(nums[i])
-                dfs(i+1, path)
-                path.removeLast()
+                path.add(nums[i]) // 현재 엘리먼트 추가
+                dfs(i+1, path) // 재귀 DFS
+                path.removeLast() // 돌아온 이후 현재 엘리먼트 삭제
             }
         }
 
-        dfs(0, ArrayDeque())
+        dfs(0, ArrayDeque()) // DFS 시작
         return results
     }
 }
