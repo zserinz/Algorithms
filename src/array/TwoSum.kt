5109e98e9568c1587030db51c0586f9eab9a2fc7
zserinz <serinn1023@gmail.com>
2024-01-02 23:36:55 +0900

    TwoSum 복습

--- src/array/TwoSum.kt
+++ src/array/TwoSum.kt
@@ -3,15 +3,17 @@ package array
 /**
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
  */
-fun twoSum(nums: IntArray, target: Int): IntArray {
-    val numsMap: MutableMap<Int, Int> = mutableMapOf()
+class TwoSum {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val numsMap: MutableMap<Int, Int> = mutableMapOf()
 
-    for ((i, num) in nums.withIndex()) {
-        if (numsMap.containsKey(target-num)) {
-            return intArrayOf(numsMap[target-num]?:0,i)
+        for ((i, num) in nums.withIndex()) {
+            if (numsMap.containsKey(target - num)) {
+                return intArrayOf(numsMap[target - num] ?: 0, i)
+            }
+            numsMap[num] = i
         }
-        numsMap[num] = i
-    }
 
-    return intArrayOf(0,0)
+        return intArrayOf(0, 0)
+    }
 }
