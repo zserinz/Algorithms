cde0f56aebc6fbce2578118d8c399880ee22b8c5
zserinz <serinn1023@gmail.com>
2024-01-24 01:39:29 +0900

    [DFS] number of islands 풀이 추가

--- src/nossi_practice/week_3_dfs_bfs/NumberOfIslands.java
+++ src/nossi_practice/week_3_dfs_bfs/NumberOfIslands.java
@@ -6,7 +6,14 @@ package nossi_practice.week_3_dfs_bfs;
  * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
  */
 public class NumberOfIslands {
-    public int numIslands(char[][] grid) {
+    static int rowLength;
+    static int colLength;
+    static boolean[][] visited;
+    static int[] dr = {0,1,0,-1};
+    static int[] dc = {1,0,-1,0};
+
+
+    public int numIslands1(char[][] grid) {
         int numIslands = 0;
         boolean[][] visited = new boolean[grid.length][grid[0].length];
 
@@ -14,7 +21,7 @@ public class NumberOfIslands {
         for (int row = 0; row < grid.length; row++) {
             for (int col = 0; col < grid[row].length; col++) {
                 if (grid[row][col] == '1' && !visited[row][col]) {
-                    dfs(grid, row, col, visited);
+                    dfs1(grid, row, col, visited);
                     numIslands++;
                 }
             }
@@ -23,15 +30,50 @@ public class NumberOfIslands {
         return numIslands;
     }
 
-    private void dfs(char[][] grid, int row, int col, boolean[][] visited) {
+    private void dfs1(char[][] grid, int row, int col, boolean[][] visited) {
         if (row < 0 || row >= grid.length || col < 0 || col >= grid[row].length || grid[row][col] == '0' || visited[row][col]) {
             return;
         }
 
         visited[row][col] = true;
-        dfs(grid, row + 1, col, visited);
-        dfs(grid, row - 1, col, visited);
-        dfs(grid, row, col + 1, visited);
-        dfs(grid, row, col - 1, visited);
+        dfs1(grid, row + 1, col, visited);
+        dfs1(grid, row - 1, col, visited);
+        dfs1(grid, row, col + 1, visited);
+        dfs1(grid, row, col - 1, visited);
+    }
+
+    public int numIslands2(char[][] grid) {
+        int count = 0;
+        rowLength = grid.length;
+        colLength = grid[0].length;
+        visited = new boolean[rowLength][colLength];
+
+        for (int i = 0; i < rowLength; i++) {
+            for (int j = 0; j < colLength; j++) {
+                if ((grid[i][j] == '1') && (!visited[i][j])) {
+                    dfs2(i, j, grid);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static boolean isValid(int r, int c, char[][] grid) {
+        return (r >= 0 && r < rowLength) && (c >= 0 && c < colLength) && (grid[r][c] == '1');
+    }
+
+    private void dfs2(int r, int c, char[][] grid) {
+        visited[r][c] = true;
+        for (int i = 0; i < 4; i++) {
+            int nextRow = r + dr[i];
+            int nextCol = c + dc[i];
+            if (isValid(nextRow, nextCol, grid)) {
+                if (!(visited[nextRow][nextCol])) {
+                    dfs2(nextRow, nextCol, grid);
+                }
+            }
+        }
     }
 }
